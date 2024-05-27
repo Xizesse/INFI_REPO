f0dc60902ac4d944ed43f03cb1a95f3382f225f8
Ana Pinto <anocas.rp@gmail.com>
2024-05-27 17:18:37 +0100

    calculate_costs() changes

--- ERP/classes/order.py
+++ ERP/classes/order.py
@@ -1,4 +1,3 @@
-from classes.raw_order import Raw_order
 import xml.etree.ElementTree as ET
 
 class Order:
@@ -34,32 +33,34 @@ class Order:
         for order in orders:
             print(order)
 
-
     def calculate_costs(self):
 
-        from classes.ProductionPlan import avg_prod_times
-        from db import get_order, get_purchasing_plan, get_dispatch_date
+        from db import get_purchasing_plan, get_dispatch_date, insert_costs
 
-        order_id = self.number
+        purchasing_plan = get_purchasing_plan(self.number)
+        
+        #dispatch_date = get_dispatch_date(self.number) 
+        
+        ## Mock data 
+        dispatch_date = 10
+        ########
 
-        order = get_order(order_id)
+        prod_time = avg_machine_busy_times[self.piece]         # Average production time for this piece
+        
+        prod_cost = prod_time * 1       # 1€ per second 
 
-        purchasing_plan = get_purchasing_plan(order_id)
-        prod_time = avg_prod_times[order.piece]
-        dispatch_date = get_dispatch_date(order_id) 
-        cost = order.calculate_costs(purchasing_plan, prod_time, dispatch_date)
+        total_cost = 0
+        for plan in purchasing_plan:    # For each raw order used for this order
 
-        prod_cost = prod_time * 1       # 1€ per hour
-
-        for plan in purchasing_plan:    #For each raw order used for this order
-            raw_order = plan.raw_order
-            raw_cost+= raw_order.price_pp * plan.quantity
-            if plan.arrival_date > arrival_date:
-                arrival_date = plan.arrival_date
-            
-        deprec_cost = raw_cost * (dispatch_date - arrival_date) * 1  # 1% depreciation per day
-        total_cost = raw_cost + prod_cost + deprec_cost
+            raw_cost = plan.raw_order.price_pp  # Raw cost per piece
+            deprec_cost = raw_cost * (dispatch_date - plan.arrival_date) * 1  # 1% depreciation per day per piece 
+                     
+            total_cost+= raw_cost + deprec_cost
+        
+        total_cost = total_cost + prod_cost * self.quantity    
         unit_cost = total_cost / self.quantity
+
+        insert_costs(self.number, total_cost, unit_cost)
 
         return total_cost, unit_cost
     
@@ -80,3 +81,11 @@ def parse_new_orders(new_orders_file):
             new_orders.append(new_order)
 
         return new_orders
+
+avg_machine_busy_times = {
+    "P5": 1.58 * 60,
+    "P6": 1.58 * 60,
+    "P7": 1 * 60,
+    "P9": 1.5 * 60,
+}
+
